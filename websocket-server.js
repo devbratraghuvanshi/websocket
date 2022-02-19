f6751f1ea497267f12f8f88ede80c6f204bb1ec5
Devbrat Raghuvanshi <developer.devbrat@gmail.com>
2022-02-20 01:07:22 +0530

    initial commit

---
+++ websocket-server.js
@@ -0,0 +1,30 @@
+const http = require('http');
+const WebSocketServer = require('websocket').server;
+
+const server = http.createServer();
+server.listen(8080);
+
+const wsServer = new WebSocketServer({
+    httpServer: server
+});
+
+
+wsServer.on('request', function(request) {
+
+    const connection = request.accept(null, request.origin);
+
+    connection.on('message', function(message) {
+        console.log('Received Message:', message.utf8Data);
+       // connection.sendUTF('Hi this is WebSocket server!');
+      });
+      var count = 0;
+      setInterval(function () {
+          count++;
+          connection.send(count);
+      }, 5000);
+
+    connection.on('close', function(reasonCode, description) {
+        console.log('Client has disconnected.');
+    });
+
+});
